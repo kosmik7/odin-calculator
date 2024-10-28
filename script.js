efd5ae9479abd5edec3fef7ddf6756e5a32ed5ee
JP Brunache <34013885+kosmik7@users.noreply.github.com>
2024-10-28 14:53:28 +0100

    Add the operate function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 };
+
+function operate(calcOperator, calcNumberA, calcNumberB) {
+    switch (calcOperator) {
+        case '+':
+            return add(calcNumberA, calcNumberB);
+        case '-':
+            return subtract(calcNumberA, calcNumberB);
+        case '*':
+            return multiply(calcNumberA, calcNumberB);
+        case '/':
+            return divide(calcNumberA, calcNumberB);
+    }
+}
+
+let calcNumberA
+let calcNumberB
+let calcOperator
